e2f4c9b3e181ca18cb5516f216679317848d63a9
Shenhaoyi <45191012+Shenhaoyi@users.noreply.github.com>
2020-07-30 23:34:08 +0800

    deploy

---
+++ precache-manifest.31f2a3c08032a00289601cb4edcc62dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a766ea4cb9cd9c50d8a5",
+    "url": "/morney-1-website/css/app.9bd917f5.css"
+  },
+  {
+    "revision": "f7532d2fb3a441f027c76797f6358137",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "a766ea4cb9cd9c50d8a5",
+    "url": "/morney-1-website/js/app.495d6edd.js"
+  },
+  {
+    "revision": "875999d2a00858da242f",
+    "url": "/morney-1-website/js/chunk-vendors.73762d7e.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
